58aaa1b8db6f2d26bcc57e11bcfe54397ee6825c
bishang <1063352967@qq.com>
2023-05-22 09:24:55 +0800

    fix:添加注释

--- src/core/App.jsx
+++ src/core/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import Context from '../component/context'//测试context
-import OldRedux from '../component/oldRdeux/Test'//测试redux
-import MobxTest from '../component/mobx/index'
-import { Provider } from 'mobx-react'
-import { Router } from 'react-router';
-import emitter from "../event";
+import React from "react";
+// import Context from "../component/context"; //测试context
+// import OldRedux from "../component/oldRdeux/Test"; //测试redux
+// import MobxTest from "../component/mobx/index";//测试mobx
+import { Provider } from "mobx-react";
+import { Router } from "react-router";
 import renderRoutes from "../utils/RouterConfig";
 import routes from "./routes";
-import Base from "../basic/App";
-import {AdminInfoContextProvider} from 'src/context/router/Router.jsx'
-const App = function ({ rootStore,history }) {
+import { AdminInfoContextProvider } from "src/context/router/Router.jsx";
+const App = function ({ rootStore, history }) {
   return (
-    <Provider rootStore={rootStore}>
-     <Router history={history}>
-      <AdminInfoContextProvider>
-      {renderRoutes(routes)}
-          {/* <Base/> */}
+    <Router history={history}>
+    <AdminInfoContextProvider>
+        <Provider rootStore={rootStore}>
+          {renderRoutes(routes)}{/* 整个系统 */}     
+          {/* <MobxTest /> */}  {/* 测试mobx */}
+          {/* <OldRedux/> */}{/* 测试redux */}
+          {/* <Context/> */} {/*测试上下文*/}
+        </Provider>
       </AdminInfoContextProvider>
-            </Router>
-    </Provider>
+    </Router>
   );
 };
 export default App;
--- src/core/index.js
+++ src/core/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createBrowserHistory } from 'history';
-import { syncHistoryWithStore } from 'mobx-react-router';
-import  App  from "./App";
-import 'antd/dist/reset.css';
-
-import store from '../store/mobx/rootStore'
-
+import { createBrowserHistory } from "history";
+import { syncHistoryWithStore } from "mobx-react-router";
+import "antd/dist/reset.css";
+import store from "../store/mobx/rootStore";
+import App from "./App";
 const browserHistory = createBrowserHistory();
 const history = syncHistoryWithStore(browserHistory, store.routing);
-ReactDOM.render(<App  rootStore={store} history={history}/>, document.getElementById("root"));
+ReactDOM.render(
+  <App rootStore={store} history={history} />,
+  document.getElementById("root")
+);
